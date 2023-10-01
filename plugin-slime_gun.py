98ad895c1830e5e7c18e90e7f856c5b7fc615407
Ccode-lang <coop112097@gmail.com>
2023-09-30 17:13:10 -0700

    guilded support

--- plugin-slime_gun.py
+++ plugin-slime_gun.py
@@ -2,12 +2,12 @@ import random
 from xander_plugin import *
 
 def onload():
-    log("Slime gun plugin loaded")
-    
-allowed_users = [993653933148995604, 837119081790046228, 886878726686666752]
+    log("Slime gun plugin loaded!")
+
+allowed_users = [993653933148995604, 837119081790046228, 886878726686666752, "dlKLOPw4", "AYNqMeG4", "4oxKNYnd"]
 gifs = ["https://tenor.com/view/shower-showering-flow-flowing-pouring-gif-12198611", "https://tenor.com/view/lasllen-nickelodeon-kca-kids-choice-awards-chris-pratt-gif-18932606", "https://tenor.com/view/tusareve-katy-perry-slime-nickelodeon-gif-25468694", "https://tenor.com/view/gunge-male-slime-nick-kca-gif-25172114", "https://tenor.com/view/slime-charlie-damelio-splash-kca2021-kids-choice-awards-gif-20736287"]
 async def onmessage(message):
-    if message.content.startswith("!slime ") and message.author.id in allowed_users:
+    if message.content.startswith("!slime "): # and message.author.id in allowed_users
         gif = gifs[random.randint(0, len(gifs) - 1)]
         try:
             await message.channel.send(f"{message.mentions[0].mention} was slimed!")
