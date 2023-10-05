ad2c73be638573bd8f2537ab9f3a868034921880
Ccode-lang <coop112097@gmail.com>
2023-10-04 19:04:29 -0700

    gtg rewrite

---
+++ plugin-gtg.py
@@ -0,0 +1,19 @@
+import robospeak as rs
+
+from xander_plugin import *
+
+def onload():
+    help_menu_edit("Gtg plugin", "A plugin that reacts to gtg messages.")
+    log("Gtg plugin loaded")
+
+async def onmessage(message):
+    if message.content.lower() == "gtg school":
+        await message.channel.send(f"*The {message.author.mention} has vanished back to the void called school.*")
+        return False
+    elif message.content.lower() == "gtg thing":
+        await message.channel.send(f"*The {message.author.mention} has vanished for unknown reasons.*")
+        return False
+    return True
+
+def onexit():
+    log("Gtg plugin exit run")
