57ac99b823640672f46f2d778c6d8f5aecb266b4
Ccode-lang <coop112097@gmail.com>
2023-10-04 19:07:46 -0700

    guilded support

--- plugin-gtg.py
+++ plugin-gtg.py
@@ -11,7 +11,10 @@ async def onmessage(message):
         await message.channel.send(f"*The {message.author.mention} has vanished back to the void called school.*")
         return False
     elif message.content.lower() == "gtg thing":
-        await message.channel.send(f"*The {message.author.mention} has vanished for unknown reasons.*")
+        if config.platform == "discord":
+            await message.channel.send(f"*The {message.author.mention} has vanished for unknown reasons.*")
+        elif config.platform == "guilded":
+            await message.channel.send(f"*The {message.author.name} has vanished for unknown reasons.*")
         return False
     return True
 
