3431857c5698316b4dd568bcf26048cf69f2f233
Ccode-lang <coop112097@gmail.com>
2023-10-05 16:39:41 -0700

    delete messages from user id

---
+++ plugin-dmsg.py
@@ -0,0 +1,32 @@
+from xander_plugin import *
+from traceback import format_exc
+
+def onload():
+    help_menu_edit("!dmsg", "A command that deletes all of a user id's messages.")
+    log("Delete messages plugin loaded!")
+
+user = None
+
+def check(ctx):
+    global user
+    return ctx.author.id == user.id
+
+async def onmessage(message):
+    global user
+    print(message.guild.channels)
+    if message.content.startswith("!dmsg "):
+        user = message.guild.get_member(int(message.content[6:]))
+        if user:
+            for channel in message.guild.channels:
+                if not type(channel) is discord.CategoryChannel:
+                    while True:
+                        deleted = await channel.purge(check=check)
+                        if deleted == []:
+                            break
+            await message.channel.send("Purged all messages from given user")
+            return False
+    return True
+
+
+def onexit():
+    log("Delete messages plugin exit run.")
